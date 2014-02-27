42d2c7fba8dd1b01b6f0389961e325a8a5c9f872
dharwath <dharwath@csail.mit.edu>
2014-02-27 16:09:18 -0500

    test

--- fast_sdtw.cpp
+++ fast_sdtw.cpp
@@ -1,50 +1,73 @@
-/* -*- C++ -*-
- *
- * Author: David Harwath
- *
- */
-
 #include <boost/program_options.hpp>
-
 using namespace boost;
 namespace po = boost::program_options;
 
 #include <iostream>
 #include <algorithm>
 #include <iterator>
-
 using namespace std;
 
+// A helper function to simplify the main part.
+template<class T>
+ostream& operator<<(ostream& os, const vector<T>& v) {
+	copy(v.begin(), v.end(), ostream_iterator<T>(cout, " "));
+	return os;
+}
 
- int main(int argc, char **argv) {
- 	po::options_description desc("Allowed options");
- 	desc.add_options()
- 		("help", "Print help message")
- 		("input-file", po::value< vector<string> >(), "input file")
- 	;
+int main(int argc, char* argv[]) {
+	try {
+		int opt;
+		int portnum;
+		
+		po::options_description desc("Allowed options");
+		desc.add_options()
+		("help", "produce help message")
+		("optimization", po::value<int>(&opt)->default_value(10),
+		 "optimization level")
+		("verbose,v", po::value<int>()->implicit_value(1),
+		 "enable verbosity (optionally specify level)")
+		("listen,l", po::value<int>(&portnum)->implicit_value(1001)
+		 ->default_value(0,"no"),
+		 "listen on a port.")
+		("include-path,I", po::value< vector<string> >(),
+		 "include path")
+		("input-file", po::value< vector<string> >(), "input file");
+		
+		po::positional_options_description p;
+		p.add("input-file", -1);
 
- 	po::positional_options_description p;
- 	p.add("input-file", -1);
+		po::variables_map vm;
+		po::store(po::command_line_parser(argc, argv).
+		options(desc).positional(p).run(), vm);
+		po::notify(vm);
 
- 	po::variables_map vm;
- 	po::store(po::command_line_parser(argc, argv).
- 			  options(desc).positional(p).run(), vm);
- 	po::notify(vm);
-
- 	if (vm.count("help")) {
- 		cout << desc << endl;
- 		return 1;
- 	}
-
-	if (vm.count("input-file")) {
-		//const vector<string> &input_files = vm["intput-file"].as< vector<string> >();
-    	cout << "Input files are: ";
-    	for (vector<string>::const_iterator it = vm["intput-file"].as< vector<string> >().begin();
-    		 it != vm["intput-file"].as< vector<string> >().end(); ++it) {
-    		cout << *it << ' ';
-    	}
-    	cout << endl;
+		if (vm.count("help")) {
+			cout << "Usage: options_description [options]\n";
+			cout << desc;
+			return 0;
+		}
+		if (vm.count("include-path"))
+		{
+			cout << "Include paths are: "
+			<< vm["include-path"].as< vector<string> >() << "\n";
+		}
+		if (vm.count("input-file"))
+		{
+			cout << "Input files are: "
+			<< vm["input-file"].as< vector<string> >() << "\n";
+		}
+		if (vm.count("verbose")) {
+			cout << "Verbosity enabled. Level is " << vm["verbose"].as<int>()
+			<< "\n";
+		}
+		cout << "Optimization level is " << opt << "\n";
+		cout << "Listen port is " << portnum << "\n";
+	}
+	catch(std::exception& e) {
+		cout << e.what() << "\n";
+		return 1;
 	}
 
- 	return 0;
- }
+return 0;
+
+}
